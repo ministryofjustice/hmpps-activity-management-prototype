76fb266fd9d8bdbba613b73051d2d47c384d4c82
ricky saunders <79845902+RickySaunders@users.noreply.github.com>
2022-08-19 14:19:30 +0100

    service name updated

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Activities',
+  serviceName: 'Activities Management',
 
   // Default port that prototype runs on
   port: '3000',
