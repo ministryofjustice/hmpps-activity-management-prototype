143184558574eadabaa7757c4072c4dcbd239dd3
ricky saunders <79845902+RickySaunders@users.noreply.github.com>
2022-09-09 15:40:01 +0100

    V1 - CHECK YOUR ANSWERS CLONE SECTION

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -173,8 +173,170 @@ router.post(version +'/create/activity-will-it-recur', function(req, res) {
 			res.redirect(version +'/create/activity-check-your-answers')
 		}
 	});
+
+	router.post(version +'/create/activity-add-education-three-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
 });
 
+
+
+
+///////CREATE CHECK YOUR ANSWERS///////////////
+router.post(version +'/create/check/activity-name', function(req, res) {
+	{
+		res.redirect(version +'/create/activity-check-your-answers')
+	}
+});
+
+router.post(version +'/create/check/activity-capacity', function(req, res) {
+	{
+		res.redirect(version +'/create/activity-check-your-answers')
+	}
+});
+
+router.post(version +'/create/check/activity-will-it-recur', function(req, res) {
+	if (req.session.data.activityWillItRecur == 'yes')
+	{
+		res.redirect(version +'/create/check/activity-add-recurrence')
+	}
+	else {
+		res.redirect(version +'/create/activity-start-date')
+	}
+
+		//Not recurring
+		router.post(version +'/create/check/activity-start-date', function(req, res) {
+			{
+				res.redirect(version +'/create/activity-check-your-answers')
+			}
+		});
+
+		//Recurring
+		router.post(version +'/create/check/activity-which-days', function(req, res) {
+			{
+				res.redirect(version +'/create/activity-check-your-answers')
+			}
+		});
+
+	router.post(version +'/create/check/activity-start-time', function(req, res) {
+		{
+			if (req.session.data.setupLocation == 'dashboard'){
+				res.redirect(version +'/create/activity-check-your-answers')
+			}
+			else{
+				res.redirect(version +'/create/activity-check-your-answers')
+			}
+		}
+	});
+
+	router.post(version +'/create/check/activity-location-list-dashboard', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-location-list-dropdown', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-incentive-level', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-risk-assessment', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	//Add another submit button
+	router.post(version +'/create/check/addAlert', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/addAlert2', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-alerts-one-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-alerts-two-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-alerts', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-payment-details', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	router.post(version +'/create/check/addEducation', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	router.post(version +'/create/check/addEducation2', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	router.post(version +'/create/check/addEducation3', function(req, res) {
+		{
+			res.redirect(version +'/create/check/activity-add-education-three-added')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-education', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-education-one-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	router.post(version +'/create/check/activity-add-education-two-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+	router.post(version +'/create/check/activity-add-education-three-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+
+	router.post(version +'/create/check/activity-add-education-three-added', function(req, res) {
+		{
+			res.redirect(version +'/create/activity-check-your-answers')
+		}
+	});
+});
+
+
+///////END CREATE CHECK YOUR ANSWERS///////////////
 
 
 ///////END CREATE//////////
