19d98cfd38507371547e558275b32839b371f68d
Dan Sensecall <dan@sensecall.co.uk>
2022-10-03 11:50:54 +0100

    Unlock app prototype

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-
-module.exports = router
 
 //CREATE
-require('./routes/create/routes-v1.js')(router);
-require('./routes/create/routes-v2.js')(router);
-require('./routes/create/routes-v3.js')(router);
-
+require('./routes/create/routes-v1')(router);
+require('./routes/create/routes-v2')(router);
+require('./routes/create/routes-v3')(router);
 
 //UNLOCK
+router.use(/\/unlock\/version-([0-9]+)/, (req, res, next) => {
+	req.version = req.originalUrl.split('/')[2]
+
+	require(`./views/unlock/version-${req.params[0]}/routes`)(req, res, next);
+})
+
+
+module.exports = router 
