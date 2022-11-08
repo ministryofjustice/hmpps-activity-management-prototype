d135c43682d874797fccfa1efd985ba36fe9960e
Dan Sensecall <dan@sensecall.co.uk>
2022-11-08 13:02:15 +0000

    attendance list modals and separate journeys plus prototype config page

--- app/views/unlock/version-2/routes.js
+++ app/views/unlock/version-2/routes.js
@@ -3,7 +3,41 @@ const router = express.Router()
 
 	//redirect the root url to the start page
 	router.get('/', function(req, res) {
-		res.redirect(req.version + '/whereabouts')
+		res.redirect(req.version + '/config')
+	});
+
+	router.post('/config', function(req, res) {
+		res.redirect('whereabouts')
+	});
+
+	router.get('/attendance-list', function(req, res) {
+		if(req.session.data['config'] && req.session.data['config']['attend-pattern'] == 'modals') {
+			res.redirect('attendance-list--modals')
+		} else {
+			res.render('unlock/' + req.version + '/attendance-list')
+		}
+	});
+	router.post('/attendance-list', function(req, res) {
+		if(req.session.data['selected-prisoners'].length > 1) {
+			res.redirect('add-attendance-details--multiple')
+		} else {
+			res.redirect('add-attendance-details--single')
+		}
+	});
+
+	router.get('/refusals-list', function(req, res) {
+		if(req.session.data['config'] && req.session.data['config']['attend-pattern'] == 'modals') {
+			res.redirect('refusals-list--modals')
+		} else {
+			res.render('unlock/' + req.version + '/refusals-list')
+		}
+	});
+	router.post('/refusals-list', function(req, res) {
+		if(req.session.data['selected-prisoners'].length > 1) {
+			res.redirect('add-attendance-details--multiple')
+		} else {
+			res.redirect('add-attendance-details--single')
+		}
 	});
 
 	router.post('/select-unlock-locations', function(req, res) {
