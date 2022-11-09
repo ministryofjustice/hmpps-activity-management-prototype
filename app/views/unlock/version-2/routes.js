aab9786dfb4382979d7fab4eb297497dbc4b92ac
Dan Sensecall <dan@sensecall.co.uk>
2022-11-09 13:42:02 +0000

    attendance stuff

--- app/views/unlock/version-2/routes.js
+++ app/views/unlock/version-2/routes.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+// app data
+const prisoners = require('../../../data/prisoners-list-1')
+
 	//redirect the root url to the start page
 	router.get('/', function(req, res) {
 		res.redirect(req.version + '/config')
 	});
 
+	// CONFIG
 	router.post('/config', function(req, res) {
 		res.redirect('whereabouts')
 	});
 
+	// ATTENDANCE LIST
 	router.get('/attendance-list', function(req, res) {
 		if(req.session.data['config'] && req.session.data['config']['attend-pattern'] == 'modals') {
 			res.redirect('attendance-list--modals')
@@ -18,13 +23,36 @@ const router = express.Router()
 		}
 	});
 	router.post('/attendance-list', function(req, res) {
-		if(req.session.data['selected-prisoners'].length > 1) {
-			res.redirect('add-attendance-details--multiple')
-		} else {
-			res.redirect('add-attendance-details--single')
-		}
+		// if(req.session.data['selected-prisoners'].length > 1) {
+		// 	res.redirect('add-attendance-details--multiple')
+		// } else {
+		// 	res.redirect('add-attendance-details--single')
+		// }
+
+		res.redirect('add-attendance-details')
 	});
 
+	// ATTENDANCE DETAILS
+	router.get('/add-attendance-details', function(req, res) {
+		let selectedPrisoners = req.session.data['selected-prisoners']
+		let filteredPrisoners = prisoners.filter(function(prisoner){
+			return selectedPrisoners.indexOf(prisoner._id) > -1;
+		});
+
+		res.render('unlock/' + req.version + '/add-attendance-details', { filteredPrisoners, selectedPrisoners })
+	});
+
+	// ATTENDANCE DETAILS MULTIPLE
+	router.get('/add-attendance-details--multiple', function(req, res) {
+		let selectedPrisoners = req.session.data['selected-prisoners']
+		let filteredPrisoners = prisoners.filter(function(prisoner){
+			return selectedPrisoners.indexOf(prisoner._id) > -1;
+		});
+
+		res.render('unlock/' + req.version + '/add-attendance-details--multiple', { filteredPrisoners, selectedPrisoners })
+	});
+
+	// REFUSALS LIST
 	router.get('/refusals-list', function(req, res) {
 		if(req.session.data['config'] && req.session.data['config']['attend-pattern'] == 'modals') {
 			res.redirect('refusals-list--modals')
@@ -40,19 +68,33 @@ const router = express.Router()
 		}
 	});
 
+	// SELECT UNLOCK LOCATIONS	
 	router.post('/select-unlock-locations', function(req, res) {
 		res.redirect('unlock-list')
 	});
 
+	// SELECT REFUSALS LOCATIONS
 	router.post('/select-refusals-locations', function(req, res) {
 		res.redirect('refusals-list')
 	});
 
+	// SELECT-ACTIVITY
+	router.get('/select-activity', function(req, res) {
+		if(req.session.data['config'] && req.session.data['config']['select-activity-pattern'] == 'table') {
+			res.redirect('select-activity-2')
+		} else {
+			res.render('unlock/' + req.version + '/select-activity')
+		}
+	});
 	router.post('/select-activity', function(req, res) {
-		req.session.data['activity-date'] = req.session.data['date'].split("/")
+		// req.session.data['activity-date'] = req.session.data['date'].split("/")
 		
 		res.redirect('attendance-list')
 	});
 	
-	
+	// SELECT-ACTIVITY-2
+	router.post('/select-activity-2', function(req, res) {		
+		res.redirect('select-activity-2__results')
+	});
+
 	module.exports = router
