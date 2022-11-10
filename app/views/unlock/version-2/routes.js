dcc9cd50204616556503175f8b0b095a00dbad39
Dan Sensecall <dan@sensecall.co.uk>
2022-11-10 13:27:25 +0000

    attend journey variants

--- app/views/unlock/version-2/routes.js
+++ app/views/unlock/version-2/routes.js
@@ -19,7 +19,11 @@ const prisoners = require('../../../data/prisoners-list-1')
 		if(req.session.data['config'] && req.session.data['config']['attend-pattern'] == 'modals') {
 			res.redirect('attendance-list--modals')
 		} else {
-			res.render('unlock/' + req.version + '/attendance-list')
+			if(req.session.data['config'] && req.session.data['config']['attendance-list-layout'] == 'sub-navigation') {
+				res.render('unlock/' + req.version + '/attendance-list-2')
+			} else {
+				res.render('unlock/' + req.version + '/attendance-list')
+			}
 		}
 	});
 	router.post('/attendance-list', function(req, res) {
