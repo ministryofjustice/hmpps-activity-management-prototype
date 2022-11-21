dbe656a3c05e276a3177a7cf1eec1a8742a477fd
ricky saunders <79845902+RickySaunders@users.noreply.github.com>
2022-11-21 15:53:04 +0000

    push

--- app/routes/create/routes-v6.js
+++ app/routes/create/routes-v6.js
@@ -145,8 +145,25 @@ router.post(version +'/create/activity-start-time-repeat', function(req, res) {
 		else {
 			res.redirect(version +'/create/activity-location-dropdown')
 		}
+	}
+});
 
+//If repeat
+router.post(version +'/create/activity-start-time-repeat2', function(req, res) {
+	{
+		if (req.session.data.activityRecurSame == 'yes')
+		{
+			res.redirect(version +'/create/activity-repeat-check')
+		}
+		else {
+			res.redirect(version +'/create/activity-repeat-check')
+		}
+	}
+});
 
+router.post(version +'/create/activity-repeat-check', function(req, res) {
+	{
+		res.redirect(version +'/create/activity-location-dropdown')
 	}
 });
 
