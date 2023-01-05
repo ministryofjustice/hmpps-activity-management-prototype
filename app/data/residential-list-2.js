daee91aabf1ad588bcfd865359d5963a1213fe1a
Dan Sensecall <dan@sensecall.co.uk>
2023-01-05 17:11:02 +0000

    unlock and refusals changes

---
+++ app/data/residential-list-2.js
@@ -0,0 +1,224 @@
+module.exports = [
+  {
+    "id": 1,
+    "name": "Houseblock 1",
+    "spurs": [
+      {
+        "id": 1,
+        "name": "Spur 1",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Spur 2",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Spur 3",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "name": "Spur 4",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          }
+        ]
+      },
+      {
+        "id": 5,
+        "name": "Spur 5",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Houseblock 2",
+    "spurs": [
+      {
+        "id": 1,
+        "name": "Spur 1",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Spur 2",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Spur 3",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          },
+          {
+            "id": 3,
+            "name": "Landing 3"
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "name": "Spur 4",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          },
+          {
+            "id": 3,
+            "name": "Landing 3"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Houseblock 3",
+    "spurs": [
+      {
+        "id": 1,
+        "name": "Spur 1",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Spur 2",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          },
+          {
+            "id": 3,
+            "name": "Landing 3"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "name": "Spur 3",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "name": "Spur 4",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          },
+          {
+            "id": 2,
+            "name": "Landing 2"
+          },
+          {
+            "id": 3,
+            "name": "Landing 3"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Segregation Unit",
+    "spurs": [
+      {
+        "id": 1,
+        "name": "Spur 1",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Spur 2",
+        "landings": [
+          {
+            "id": 1,
+            "name": "Landing 1"
+          }
+        ]
+      }
+    ]
+  }
+]
