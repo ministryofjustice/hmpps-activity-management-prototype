2bfd4c7b053fdb040a24e031b17f78d0bb41aa2b
brian-milman-justice <brian.milman@digital.justice.gov.uk>
2023-01-05 13:15:01 +0000

    Appointments - Version 3 created

---
+++ app/views/appointments/version-3/routes.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+
+	//redirect the root url to the start page
+	router.get('/', function(req, res) {
+		res.redirect(req.version + '/start')
+	});
+
+	
+	
+
+	router.post(`/group-appointments/add-people-route`, function (req, res) {
+		const peopleRoute = req.session.data['add-people-method'];
+		if (peopleRoute === 'upload-file') {
+			res.redirect(`upload-file`);
+		} else {
+			res.redirect(`search-person`);
+		}
+	  });
+
+
+
+
+
+	  router.post(`/group-appointments/single-or-multiple`, function (req, res) {
+		const schedType = req.session.data['schedule-type'];
+		if (schedType === 'single-time') {
+			res.redirect(`appointment-time`);
+		} else {
+			res.redirect(`schedule-builder`);
+		}
+	  });
+
+	  router.post(`/group-appointments/appointment-repeat-option`, function (req, res) {
+		const appRepeat = req.session.data['appointment-repeat'];
+		if (appRepeat === 'Yes') {
+			res.redirect(`appointment-frequency`);
+		} else {
+			res.redirect(`how-to-schedule-group`);
+		}
+	  });
+
+	  
+module.exports = router
