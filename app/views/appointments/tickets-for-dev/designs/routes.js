83fa6d95951f698254ad5a4361b66a81a46461c6
brian-milman-justice <brian.milman@digital.justice.gov.uk>
2023-02-27 15:13:25 +0000

    bug fixed

--- app/views/appointments/tickets-for-dev/designs/routes.js
+++ app/views/appointments/tickets-for-dev/designs/routes.js
@@ -10,26 +10,13 @@ const router = express.Router()
 
   router.post(`/recurring-single-appointment/appointment-repeat-question`, function (req, res) {
 	const appointRepeat = req.session.data['appointment-repeat'];
-	if (appointRepeat === 'Yes') {
-		res.redirect(`appointment-frequency`);
-	} else {
+	if (appointRepeat === 'No') {
 		res.redirect(`check-answers`);
+	} else {
+		res.redirect(`appointment-frequency`);
 	}
   });
 
-  router.post('/recurring-single-appointment/date-and-time', function(req, res) {
-	{
-		//Change numerical month to short month name
-		req.session.data.month = getMonthName(req.session.data.month)
-		req.session.data.dateofappointment = req.session.data.day + " " + req.session.data.month + " " + req.session.data.year
-		
-	}
-});
-
-function getMonthName(monthNumber) {
-const date = new Date();
-date.setMonth(monthNumber - 1);
-return date.toLocaleString('en-US', { month: 'long' });
-}
+  
 	  
 module.exports = router
