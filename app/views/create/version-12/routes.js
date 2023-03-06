3925bb812ee957b872613515e4629f4f14758192
Dan Sensecall <dan@sensecall.co.uk>
2023-03-06 11:17:56 +0000

    journey

--- app/views/create/version-12/routes.js
+++ app/views/create/version-12/routes.js
@@ -43,5 +43,94 @@ router.post('/create-activity/activity-name', function (req, res) {
 router.get('/create-activity/risk-assessment-levels', function (req, res) {
     res.render('create/' + req.version + '/risk-assessment-levels')
 });
-    
+// redirect to payment details page
+router.post('/create-activity/risk-assessment-levels', function (req, res) {
+    res.redirect('payment-details')
+});
+
+// payment details page
+router.get('/create-activity/payment-details', function (req, res) {
+    res.render('create/' + req.version + '/payment-details')
+});
+// redirect to payment details check page
+router.post('/create-activity/payment-details', function (req, res) {
+    res.redirect('payment-details-list')
+});
+
+// payment details check page
+router.get('/create-activity/payment-details-list', function (req, res) {
+    res.render('create/' + req.version + '/payment-details-list')
+});
+// redirect to education level page
+router.post('/create-activity/payment-details-list', function (req, res) {
+    res.redirect('education-level-check')
+});
+
+// education level check page
+router.get('/create-activity/education-level-check', function (req, res) {
+    res.render('create/' + req.version + '/education-level-check')
+});
+// logic for education level check page
+router.post('/create-activity/education-level-check', function (req, res) {
+    if (req.session.data['education-level-check'] === 'yes') {
+        res.redirect('select-education-level')
+    } else {
+        res.redirect('start-date')
+    }
+});
+
+// select education level page
+router.get('/create-activity/select-education-level', function (req, res) {
+    res.render('create/' + req.version + '/select-education-level')
+});
+// redirect to education level list page
+router.post('/create-activity/select-education-level', function (req, res) {
+    res.redirect('education-level-list')
+});
+
+// education level list page
+router.get('/create-activity/education-level-list', function (req, res) {
+    res.render('create/' + req.version + '/education-level-list')
+});
+// redirect to start date page
+router.post('/create-activity/education-level-list', function (req, res) {
+    res.redirect('start-date')
+});
+
+// start date page
+router.get('/create-activity/start-date', function (req, res) {
+    res.render('create/' + req.version + '/start-date')
+});
+// redirect to end date check page
+router.post('/create-activity/start-date', function (req, res) {
+    res.redirect('end-date-check')
+});
+
+// end date check page
+router.get('/create-activity/end-date-check', function (req, res) {
+    res.render('create/' + req.version + '/end-date-check')
+});
+// redirect logic for end date check page
+router.post('/create-activity/end-date-check', function (req, res) {
+    if (req.session.data['end-date-check'] === 'yes') {
+        res.redirect('end-date')
+    } else {
+        res.redirect('days-and-times')
+    }
+});
+
+// end date page
+router.get('/create-activity/end-date', function (req, res) {
+    res.render('create/' + req.version + '/end-date')
+});
+// redirect to days and times page
+router.post('/create-activity/end-date', function (req, res) {
+    res.redirect('days-and-times')
+});
+
+// days and times page
+router.get('/create-activity/days-and-times', function (req, res) {
+    res.render('create/' + req.version + '/days-and-times')
+});
+
 module.exports = router
