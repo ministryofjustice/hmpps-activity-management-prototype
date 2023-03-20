ec0c9a4c164c1b7dbac4c663f206f2c58a8e4463
Dan Sensecall <dan@sensecall.co.uk>
2023-03-20 09:42:59 +0000

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage activities and schedules',
+  serviceName: 'Activities Management prototype',
 
   // Default port that prototype runs on
   port: '3000',
