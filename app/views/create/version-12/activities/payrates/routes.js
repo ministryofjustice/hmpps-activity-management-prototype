befa611d87422cb266bc6bedf5891fdffd7edd05
Dan Sensecall <dan@sensecall.co.uk>
2023-04-24 16:10:30 +0100

    payrates done
    missing:
    - single person with multiple alternative rates
    - flat rates stuff

--- app/views/create/version-12/activities/payrates/routes.js
+++ app/views/create/version-12/activities/payrates/routes.js
@@ -33,6 +33,11 @@ router.get("/", function (req, res) {
     noAlternativePayRates = true;
   }
 
+  let hasFlatRate = false;
+  if (payRatesByIncentiveLevel["flatRate"].length > 0) {
+    hasFlatRate = true;
+  }
+
   // sum the number of prisoners allocated to each payrate and add it to the payratesByIncentiveLevel object
   let prisoners = req.session.data["timetable-complete-1"]["prisoners"];
   let currentlyAllocatedPrisoners = prisoners.filter(
@@ -66,6 +71,7 @@ router.get("/", function (req, res) {
     payRatesByIncentiveLevel,
     noAlternativePayRates,
     currentPage: "pay-rates",
+    hasFlatRate,
   });
 });
 
@@ -197,17 +203,55 @@ router.get("/delete/:payRateId", function (req, res) {
 
   console.log(prisonersOnPayRate);
 
+  // if (prisonersOnPayRate != undefined && prisonersOnPayRate.length > 1) {
+  //   let prisonerIds = prisonersOnPayRate.join(",");
+
+  //   // redirect to the check prisoners page with this payrate id as a url param
+  //   res.redirect(payRate.payRate_id + "/check-prisoners/" + prisonerIds);
+  // } else if (prisonersOnPayRate != undefined && prisonersOnPayRate.length === 1) {
+  //   let prisonerId = prisonersOnPayRate[0];
+
+  //   // redirect to the check prisoner page with this payrate id as a url param
+  //   res.redirect(payRate.payRate_id + "/prisoner-action/" + prisonerId);
+  // } else {
+  //   // render the delete payrate page
+  //   res.render(req.protoUrl + "/delete", {
+  //     activity,
+  //     payRate,
+  //   });
+  // }
+
+  let suitablePayRates = getSuitablePayrates(activity, payRateId);
+  let prisonerIds = prisonersOnPayRate.join(",");
+
+  // if there are multiple prisoners on the payrate, but only one suitable alternative payrate then redirect to the prisoner action list page
   if (prisonersOnPayRate != undefined && prisonersOnPayRate.length > 1) {
-    let prisonerIds = prisonersOnPayRate.join(",");
+    // if there is only one suitable payrate
+    if (suitablePayRates.length === 1) {
+      req.session.data["prisoner-action"] = "move-to-payrate";
+      req.session.data["new-payrate-id"] = suitablePayRates[0].payRate_id;
 
-    // redirect to the check prisoners page with this payrate id as a url param
-    res.redirect(payRate.payRate_id + "/check-prisoners/" + prisonerIds);
-  } else if (prisonersOnPayRate != undefined && prisonersOnPayRate.length === 1) {
-    let prisonerId = prisonersOnPayRate[0];
+      // redirect to the check prisoners list page
+      res.redirect(payRate.payRate_id + "/confirm-payrate-change/" + prisonerIds);
+    } else {
+      // if there are multiple suitable payrates
+      // redirect to the check prisoners page
+      res.redirect(payRate.payRate_id + "/check-prisoners/" + prisonerIds);
+    }
+  }
+  // if there is only one prisoner on the payrate
+  else if (prisonersOnPayRate != undefined && prisonersOnPayRate.length === 1) {
+    if (suitablePayRates.length === 1) {
+      req.session.data["prisoner-action"] = "move-to-payrate";
+      req.session.data["new-payrate-id"] = suitablePayRates[0].payRate_id;
+      let prisonerId = prisonersOnPayRate[0];
 
-    // redirect to the check prisoner page with this payrate id as a url param
-    res.redirect(payRate.payRate_id + "/prisoner-action/" + prisonerId);
-  } else {
+      // redirect to the check prisoner page for this prisoner
+      res.redirect(payRate.payRate_id + "/confirm-payrate-change/" + prisonerId);
+    }
+  }
+  // if there are no prisoners on the payrate
+  else {
     // render the delete payrate page
     res.render(req.protoUrl + "/delete", {
       activity,
@@ -241,6 +285,29 @@ router.post("/delete/:payRateId", function (req, res) {
   };
 
   res.redirect("../../payrates");
+});
+
+// check prisoners list page
+router.get("/delete/:payRateId/check-prisoners-list/:prisonerIds", function (req, res) {
+  let activityId = req.activityId;
+  let activity = req.session.data["timetable-complete-1"]["activities"].find(
+    (activity) => activity.id.toString() === activityId.toString()
+  );
+
+  let payRateId = req.params.payRateId;
+  let payRate = activity.payRates.find((payRate) => payRate.payRate_id.toString() === payRateId.toString());
+
+  let prisonerIds = req.params.prisonerIds.split(",");
+  let prisoners = req.session.data["timetable-complete-1"]["prisoners"];
+  let prisonerData = prisoners.filter((prisoner) => prisonerIds.includes(prisoner.id));
+
+  // render the check prisoners list page
+  res.render(req.protoUrl + "/check-prisoners-list", {
+    activity,
+    payRate,
+    prisonerIds,
+    prisonerData,
+  });
 });
 
 // edit payrate page
@@ -502,7 +569,6 @@ router.get("/delete/:payRateId/confirm-deallocate/:prisonerIds", function (req, 
   });
 });
 
-
 // confirm prisoner actions page
 router.get("/delete/:payRateId/confirm-prisoner-actions/:prisonerIds", function (req, res) {
   let prisoners = req.session.data["timetable-complete-1"]["prisoners"];
@@ -546,7 +612,6 @@ router.get("/delete/:payRateId/confirm-prisoner-actions/:prisonerIds", function 
     // add the prisoner to the prisonerActions array
     prisonerActions.push(prisoner);
   });
-
 
   // split the prisonerActions array into two arrays:
   // one for prisoners who are being moved and one for prisoners who are being deallocated
