3e1386a6b6ccd0eefc00c595da28e84fdc758be4
Dan Sensecall <dan@sensecall.co.uk>
2023-04-24 13:56:33 +0100

    mostly working

--- app/views/create/version-12/activities/payrates/routes.js
+++ app/views/create/version-12/activities/payrates/routes.js
@@ -340,7 +340,7 @@ router.post("/delete/:payRateId/check-prisoners/:prisonerIds", function (req, re
     // set the payrate id in the session data
     req.session.data["new-payrate-id"] = suitablePayrates[0].payRate_id;
     // redirect to the confirm deallocate page
-    res.redirect("../confirm-payrate/" + req.params.prisonerIds);
+    res.redirect("../confirm-payrate-change/" + req.params.prisonerIds);
   } else if (req.session.data["prisoner-action"] === "remove-from-activity") {
     // redirect to the confirm deallocate page
     res.redirect("../confirm-deallocate/" + req.params.prisonerIds);
@@ -394,11 +394,11 @@ router.post("/delete/:payRateId/select-payrate/:prisonerIds", function (req, res
   let newPayRateId = req.session.data["payrate-id"];
 
   // redirect to the confirm payrate page
-  res.redirect("../confirm-payrate/" + req.params.prisonerIds);
+  res.redirect("../confirm-payrate-change/" + req.params.prisonerIds);
 });
 
 // confirm payrate page
-router.get("/delete/:payRateId/confirm-payrate/:prisonerIds", function (req, res) {
+router.get("/delete/:payRateId/confirm-payrate-change/:prisonerIds", function (req, res) {
   let activityId = req.activityId;
   let activity = req.session.data["timetable-complete-1"]["activities"].find(
     (activity) => activity.id.toString() === activityId.toString()
@@ -412,7 +412,7 @@ router.get("/delete/:payRateId/confirm-payrate/:prisonerIds", function (req, res
   let prisonersOnPayRate = req.params.prisonerIds.split(",");
   let prisonerData = prisoners.filter((prisoner) => prisonersOnPayRate.includes(prisoner.id.toString()));
 
-  res.render(req.protoUrl + "/confirm-payrate", {
+  res.render(req.protoUrl + "/confirm-payrate-change", {
     activityId: req.activityId,
     activity,
     payRateId: req.params.payRateId,
@@ -427,7 +427,7 @@ router.get("/delete/:payRateId/confirm-payrate/:prisonerIds", function (req, res
 });
 
 // post route for confirm payrate page
-router.post("/delete/:payRateId/confirm-payrate/:prisonerIds", function (req, res) {
+router.post("/delete/:payRateId/confirm-payrate-change/:prisonerIds", function (req, res) {
   let activityId = req.activityId;
   let activity = req.session.data["timetable-complete-1"]["activities"].find(
     (activity) => activity.id.toString() === activityId.toString()
@@ -483,6 +483,25 @@ router.post("/delete/:payRateId/prisoner-action/:prisonerIds", function (req, re
   // redirect to the confirm prisoner actions page
   res.redirect("../confirm-prisoner-actions/" + req.params.prisonerIds);
 });
+
+// confirm deallocate page
+router.get("/delete/:payRateId/confirm-deallocate/:prisonerIds", function (req, res) {
+  let prisoners = req.session.data["timetable-complete-1"]["prisoners"];
+  let prisonersOnPayRate = req.params.prisonerIds.split(",");
+  let prisonerData = prisoners.filter((prisoner) => prisonersOnPayRate.includes(prisoner.id.toString()));
+
+  let activityId = req.activityId;
+  let activity = req.session.data["timetable-complete-1"]["activities"].find(
+    (activity) => activity.id.toString() === activityId.toString()
+  );
+
+  // render the confirm deallocate page
+  res.render(req.protoUrl + "/confirm-deallocate", {
+    prisonerData,
+    activity,
+  });
+});
+
 
 // confirm prisoner actions page
 router.get("/delete/:payRateId/confirm-prisoner-actions/:prisonerIds", function (req, res) {
