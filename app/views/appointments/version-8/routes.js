bf860e7bdbd2cffb7c87d18ab597414bfff005c5
jamesmckechnie <34271054+jamesmckechnie@users.noreply.github.com>
2023-05-03 08:13:31 +0100

    V8 prototype changes

--- app/views/appointments/version-8/routes.js
+++ app/views/appointments/version-8/routes.js
@@ -106,6 +106,16 @@ router.post(`/bulk-appointments/bulk-more-people`, function (req, res) {
 	}
   });
 
+
+  router.post(`/group-appointments/add-more-people-all-removed`, function (req, res) {
+	const peoleRepeat = req.session.data['remove-last-person-question'];
+	if (peoleRepeat === 'No') {
+		res.redirect(`dps-home`);
+	} else {
+		res.redirect(`upload-or-per-person`);
+	}
+  });
+
   router.post(`../../tickets-for-dev/designs/recurring-single-appointment/appointment-repeat-question`, function (req, res) {
 	const appointRepeat = req.session.data['appointment-repeat'];
 	if (appointRepeat === 'Yes') {
